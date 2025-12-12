40207605330f6289652860dc187700c87f4fb26c
Yop Xue <yopxue@MacBook-Pro.home>
2025-12-13 00:45:42 +0100

    first function created

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def health():
-    return {'status':'ok'}
+
+tasks = []
+
+@app.post('/')
+def create_task(id, title, description, status, priority, created_at):
+    tasks.append({
+        "id" : str(id),
+        "title" : title,
+        "description" : description,
+        "status" : status,
+        "priority" : priority,
+        "created_at" : created_at 
+    })
+    
