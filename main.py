cc2000af701ba44c8994ccc7a9c8e81e0dda0476
Yop Xue <yopxue@Yops-MBP.home>
2025-12-13 01:34:03 +0100

    added new class for TaskUpdate

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from pydantic import BaseModel 
 import uuid 
 from fastapi import HTTPException
+from typing import Optional 
+
 
 app = FastAPI()
 
-class Task(BaseModel):
+class TaskCreate(BaseModel):
     title : str
     description : str
     status : str
@@ -17,7 +19,7 @@ tasks = {}
 # changed from list to dictionary for better id lookup 
 
 @app.post('/tasks')
-def create_task(task : Task):
+def create_task(task : TaskCreate):
     task_id = str(uuid.uuid4())
 
     new_task = {
@@ -47,10 +49,46 @@ def get_tasks():
 
 
 @app.delete('/tasks/{task_id}')
-def delete_task(task_id : str):
+def delete_task(task_id: str):
     if task_id in tasks:
         del tasks[task_id]
         return 
 
     else:
-        raise HTTPException(status_code = 404, detail='Task not found')
+        raise HTTPException(status_code = 404, detail='Task not found
+
+
+
+# create new model for task update 
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[str] = None
+    priority: Optional[str] = None 
+
+
+@app.patch('/tasks/{task_id}')
+def update_task_by_id(task_id: str, updates: TaskUpdate):
+
+    if task_id not in tasks:
+        raise HTTPException(status_code = 404, detail = 'Task not found.')
+
+    task = tasks[task_id]
+
+    if updates.title is not None:
+        task['title'] = updates.title
+    
+    if updates.description is not None:
+        task['description'] = updates.description
+
+    if updates.status is not None:
+        task['status'] = updates.status
+    
+    if updates.priority is not None:
+        task['priority'] = updates.priority
+
+    task['modified_at'] = datetime.utcnow().isoformat()
+
+
+    return task
+
